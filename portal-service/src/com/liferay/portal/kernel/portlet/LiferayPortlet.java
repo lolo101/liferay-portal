b812f1d8b0bd34f818152f65f7ec5e87dd94b41e
lolo101 <lbroquet@online.fr>
2015-03-28 22:09:21 +0100

    When an Action method raises an exception, forward that exception rather than an irrelevant NoSuchMethodException.

--- portal-service/src/com/liferay/portal/kernel/portlet/LiferayPortlet.java
+++ portal-service/src/com/liferay/portal/kernel/portlet/LiferayPortlet.java
@@ -179,7 +179,7 @@ public class LiferayPortlet extends GenericPortlet {
 				return true;
 			}
 			catch (Exception e) {
-				throw new PortletException(nsme);
+				throw new PortletException(e);
 			}
 		}
 		catch (InvocationTargetException ite) {
